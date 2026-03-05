2ca5fd36c7924c0254099aacf7bc471d499d17e1
LAPTOP-J20BGGNG\taida <taidangduc1503@gmail.com>
2026-03-05 09:59:15 +0700

    feat: implement code image processing with azure functions and azure queue storage

--- src/Backend/AzureFunctions/Program.cs
+++ src/Backend/AzureFunctions/Program.cs
@@ -1,7 +1,12 @@
+using Application;
+using Infrastructure.Imaging;
+using Infrastructure.Storage;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Builder;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Persistence;
 
 var builder = FunctionsApplication.CreateBuilder(args);
 
@@ -12,5 +17,14 @@ builder.ConfigureFunctionsWebApplication();
 builder.Services
     .AddApplicationInsightsTelemetryWorkerService()
     .ConfigureFunctionsApplicationInsights();
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")!;
+var storageOptions = builder.Configuration.GetSection("Storage").Get<StorageOptions>() ?? new StorageOptions();
+
+builder.Services
+    .AddPersistence(connectionString)
+    .AddApplicationServices()
+    .AddStorage(storageOptions)
+    .AddSingleton<ImageProcessingService>();
 
 builder.Build().Run();
