4cf9271d5fed377a79382c2237935bb59f8af2cc
taidangduc <taidangduc1503@gmail.com>
2026-04-19 20:35:48 +0700

    feat: add polling refresh data

--- src/UI/reactjs/src/features/Storage/index.tsx
+++ src/UI/reactjs/src/features/Storage/index.tsx
@@ -1,204 +1,203 @@
-import { Container, useDrawer } from "@chakra-ui/react"
+import { Container, useDrawer } from "@chakra-ui/react";
 import { FileUploadDrawer, StorageHeader, StorageTable } from "./components";
 import { toaster } from "../../components/ui/toaster";
 import { useStorage } from "./hooks/useStorage";
-import { uploadFile, deleteFile, deleteFiles, downloadFile, getSignedUrl } from "./services/storage-service";
-import { useState } from "react";
+import {
+  uploadFile,
+  deleteFile,
+  deleteFiles,
+  downloadFile,
+  getSignedUrl,
+} from "./services/storage-service";
+import { useState, useEffect } from "react";
 
 export function StoragePage() {
+  const { data, refresh } = useStorage();
+  const [selection, setSelection] = useState<string[]>([]);
 
-    const { data, refresh } = useStorage();
-    const [selection, setSelection] = useState<string[]>([]);
+  const drawer = useDrawer();
 
-    const drawer = useDrawer();
+  // ref: https://www.chakra-ui.com/docs/components/toast#update
+  const handleUpload = async (files: File[]) => {
+    if (!files || files.length === 0) return;
 
-    // ref: https://www.chakra-ui.com/docs/components/toast#update
-    const handleUpload = async (files: File[]) => {
+    const toastId = toaster.create({
+      title: "Uploading...",
+      description: `Uploading ${files.length} file(s)`,
+      type: "loading",
+    });
 
-        if (!files || files.length === 0) return;
+    const results = await Promise.allSettled(
+      files.map((file) => uploadFile(file)),
+    );
 
-        const toastId = toaster.create({
-            title: "Uploading...",
-            description: `Uploading ${files.length} file(s)`,
-            type: "loading",
+    const successCount = results.filter((r) => r.status === "fulfilled").length;
+    const failCount = results.filter((r) => r.status === "rejected").length;
+
+    if (successCount === files.length) {
+      toaster.update(toastId, {
+        title: "Success",
+        description: `${successCount} file(s) uploaded`,
+        type: "success",
+      });
+    } else if (failCount > 0 && successCount > 0) {
+      toaster.update(toastId, {
+        title: "Partial Success",
+        description: `${successCount} file(s) uploaded, ${failCount} file(s) failed`,
+        type: "warning",
+      });
+    } else {
+      toaster.update(toastId, {
+        title: "Error",
+        description: `Failed to upload ${failCount} file(s)`,
+        type: "error",
+      });
+    }
+  };
+
+  const handleRefresh = () => {
+    refresh();
+  };
+
+  const handleDelete = async (id: string) => {
+    await deleteFile(id)
+      .then(() => {
+        toaster.create({
+          description: "File deleted successfully",
+          type: "success",
         });
-
-        const results = await Promise.allSettled(
-            files.map((file) => uploadFile(file))
-        );
-
-        const successCount = results.filter(r => r.status === "fulfilled").length;
-        const failCount = results.filter(r => r.status === "rejected").length;
-
-        if (successCount === files.length) {
-            toaster.update(toastId, {
-                title: "Success",
-                description: `${successCount} file(s) uploaded`,
-                type: "success",
-            });
-        }
-
-        else if (failCount > 0 && successCount > 0) {
-            toaster.update(toastId, {
-                title: "Partial Success",
-                description: `${successCount} file(s) uploaded, ${failCount} file(s) failed`,
-                type: "warning",
-            });
-        }
-
-        else {
-            toaster.update(toastId, {
-                title: "Error",
-                description: `Failed to upload ${failCount} file(s)`,
-                type: "error",
-            });
-        }
-
-    }
-
-    const handleRefresh = () => {
         refresh();
+      })
+      .catch(() => {
+        toaster.create({
+          description: "Error deleting file",
+          type: "error",
+        });
+      });
+  };
+
+  const handleBulkDelete = async () => {
+    if (selection.length === 0) {
+      return;
     }
 
-    const handleDelete = async (id: string) => {
-        await deleteFile(id)
-            .then(() => {
-                toaster.create({
-                    description: "File deleted successfully",
-                    type: "success",
-                });
-                refresh();
-            })
-            .catch(() => {
-                toaster.create({
-                    description: "Error deleting file",
-                    type: "error",
-                });
-            });
-    }
+    await deleteFiles(selection)
+      .then(() => {
+        toaster.create({
+          description: "Files deleted successfully",
+          type: "success",
+        });
+        setSelection([]);
+        refresh();
+      })
+      .catch(() => {
+        toaster.create({
+          description: "Error deleting files",
+          type: "error",
+        });
+      });
+  };
 
-    const handleBulkDelete = async () => {
+  // ref: https://bobbyhadz.com/blog/download-file-using-axios
+  // Header:
+  // {
+  //   Content-Type: application/octet-stream
+  //   Content-Disposition: attachment; filename="example.jpg"
+  //   Body: <binary stream>
+  // }
 
-        if (selection.length === 0) {
-            return;
+  const handleDownloadImage = async (id: string) => {
+    await downloadFile(id)
+      .then((response) => {
+        const href = window.URL.createObjectURL(response.data);
+
+        const anchorElement = document.createElement("a");
+        anchorElement.href = href;
+
+        const disposition = response.headers["content-disposition"];
+
+        let fileName = "file-download";
+
+        if (disposition && disposition.includes("filename=")) {
+          fileName = disposition
+            .split("filename=")[1]
+            .split(";")[0]
+            .replace(/"/g, "");
         }
 
-        await deleteFiles(selection)
-            .then(() => {
-                toaster.create({
-                    description: "Files deleted successfully",
-                    type: "success",
-                });
-                setSelection([]);
-                refresh();
-            })
-            .catch(() => {
-                toaster.create({
-                    description: "Error deleting files",
-                    type: "error",
-                });
-            });
+        anchorElement.download = fileName;
+
+        document.body.appendChild(anchorElement);
+        anchorElement.click();
+
+        document.body.removeChild(anchorElement);
+        window.URL.revokeObjectURL(href);
+      })
+      .catch(() => {
+        toaster.create({
+          description: "Error downloading file",
+          type: "error",
+        });
+      });
+  };
+
+  const handlePreview = async (id: string) => {
+    try {
+      const response = await getSignedUrl(id);
+
+      console.log("Signed URL:", response.data);
+
+      if (!response.data) {
+        throw new Error("Invalid signed URL");
+      }
+
+      window.open(response.data);
+    } catch {
+      toaster.create({
+        description: "Error previewing image",
+        type: "error",
+      });
     }
+  };
 
-    // ref: https://bobbyhadz.com/blog/download-file-using-axios 
-    // Header:
-    // {
-    //   Content-Type: application/octet-stream
-    //   Content-Disposition: attachment; filename="example.jpg"
-    //   Body: <binary stream>
-    // }
+  const handleSelect = (id: string, checked: boolean) => {
+    setSelection((prev) =>
+      checked ? [...prev, id] : prev.filter((x) => x !== id),
+    );
+  };
 
-    const handleDownloadImage = async (id: string) => {
-        await downloadFile(id)
-            .then((response) => {
-                const href = window.URL.createObjectURL(response.data);
+  const handleSelectAll = (checked: boolean) => {
+    setSelection(checked ? data.map((x) => x.id) : []);
+  };
 
-                const anchorElement = document.createElement("a");
-                anchorElement.href = href;
+  // polling refresh data every 10 seconds
+  useEffect(() => {
+    const polling_query = setInterval(() => {
+      refresh();
+    }, 10000);
+    return () => clearInterval(polling_query);
+  }, []);
 
-                const disposition = response.headers["content-disposition"];
-
-                let fileName = "file-download";
-
-                if (disposition && disposition.includes("filename=")) {
-                    fileName = disposition
-                        .split("filename=")[1]
-                        .split(";")[0]
-                        .replace(/"/g, "");
-                }
-
-                anchorElement.download = fileName;
-
-                document.body.appendChild(anchorElement);
-                anchorElement.click();
-
-                document.body.removeChild(anchorElement);
-                window.URL.revokeObjectURL(href);
-
-            })
-            .catch(() => {
-                toaster.create({
-                    description: "Error downloading file",
-                    type: "error",
-                });
-            });
-    }
-
-    const handlePreview = async (id: string) => {
-        try {
-            const response = await getSignedUrl(id);
-
-            console.log("Signed URL:", response.data);
-
-            if (!response.data) {
-                throw new Error("Invalid signed URL");
-            }
-
-            window.open(response.data);
-
-        } catch {
-            toaster.create({
-                description: "Error previewing image",
-                type: "error",
-            });
-        }
-    }
-
-    const handleSelect = (id: string, checked: boolean) => {
-        setSelection((prev) =>
-            checked
-                ? [...prev, id]
-                : prev.filter(x => x !== id))
-    }
-
-    const handleSelectAll = (checked: boolean) => {
-        setSelection(
-            checked
-                ? data.map(x => x.id)
-                : []
-        )
-    }
-
-    return (
-        <>
-            <Container pt={4} maxWidth="1000px" color="black">
-                <StorageHeader
-                    onShowDrawer={() => drawer.setOpen(true)}
-                    onRefresh={handleRefresh}
-                    onBulkDelete={handleBulkDelete}
-                    selection={selection}
-                />
-                <StorageTable
-                    items={data}
-                    selection={selection}
-                    onSelect={handleSelect}
-                    onSelectAll={handleSelectAll}
-                    onPreview={handlePreview}
-                    onDownload={handleDownloadImage}
-                    onDelete={handleDelete}
-                />
-                <FileUploadDrawer drawer={drawer} onUpload={handleUpload} />
-            </Container>
-        </>
-    )
+  return (
+    <>
+      <Container pt={4} maxWidth="1000px" color="black">
+        <StorageHeader
+          onShowDrawer={() => drawer.setOpen(true)}
+          onRefresh={handleRefresh}
+          onBulkDelete={handleBulkDelete}
+          selection={selection}
+        />
+        <StorageTable
+          items={data}
+          selection={selection}
+          onSelect={handleSelect}
+          onSelectAll={handleSelectAll}
+          onPreview={handlePreview}
+          onDownload={handleDownloadImage}
+          onDelete={handleDelete}
+        />
+        <FileUploadDrawer drawer={drawer} onUpload={handleUpload} />
+      </Container>
+    </>
+  );
 }
